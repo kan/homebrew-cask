2eadd3a02284058cd53cbadf72b40aac11c15224
Kan Fushihara <kan.fushihara@gmail.com>
2014-09-12 03:46:12 +0900

    first commit

---
+++ Casks/macuim.rb
@@ -0,0 +1,9 @@
+class Macuim < Cask
+  version '0.6.16-1'
+  sha256 'bf753f30dfcc48c97f4550091f2c3c6d0ceca35320b43f1469771fca90040f94'
+
+  url 'https://macuim.googlecode.com/files/MacUIM-0.6.16-1.dmg'
+  homepage 'https://code.google.com/p/macuim/'
+
+  pkg 'MacUIM-0.6.16-1.pkg'
+end
