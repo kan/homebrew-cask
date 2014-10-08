754b45ebc05e21cda538c2afabdd821e22f5af2e
Kan Fushihara <kan.fushihara@gmail.com>
2014-10-08 21:16:30 +0900

    Yosemite対応AquaSKKを追加

---
+++ Casks/aquaskk.rb
@@ -0,0 +1,9 @@
+class AquaSKK < Cask
+  version '4.2.1b1'
+  sha256 '623b4a9ce88c219817fa87f859e19c527bb3fbd6ed172fb5a95449c8fa01b6d3'
+
+  url 'https://github.com/codefirst/aquaskk/releases/download/v4.2.1b1/AquaSKK-20140806.dmg'
+  homepage 'http://mzp.hatenablog.com/entry/2014/08/10/110223'
+
+  pkg 'AquaSKK.pkg'
+end
